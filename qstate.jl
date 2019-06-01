a010d7fa3056374cba0c589945c8cae7a2a67bf6
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-06-01 15:03:49 -0400

    jun 1st backup

--- qstate.jl
+++ qstate.jl
@@ -10,37 +10,57 @@ abstract type QState end
 
 struct MPSState <: QState
 	s::MPS
+	freelist:: Vector{Index} #?? is it really needed
 
-	MPSState() = new(MPS())
-	# # MPSState(N::Int) = new(MPS(spinHalfSites(N)))
-	# MPSState(N::Int) = new(MPS(Int, InitState(spinHalfSites(N), 1)))
-	function MPSState(N::Int) 
-		m = MPS(spinHalfSites(N))
-		for i = 1:N
-			fill!(m[i],1.0)
-			m[i]/=norm(m[i])
+	# MPSState() = new(MPS())
+
+	function MPSState(N::Int, init::Vector{T}) where {T}
+		linklist = Index[]
+		freelist = Index[]
+		itensorlist = ITensor[]
+		if length(init)!=2
+			error("init dim error")
 		end
-		new(m)
+		if norm(init)!== 1.0 
+			error("init wrong norm")
+		end
+		for i = 1 : N 
+			push!(freelist,Index(2))
+			if i < N
+				push!(linklist,Index(1))
+			end
+			if i == 1
+				push!(itensorlist, ITensor(init, linklist[i],freelist[i]))
+			elseif i == N
+				push!(itensorlist,ITensor(init, linklist[i-1],freelist[i]))
+			else
+				push!(itensorlist,ITensor(init,linklist[i-1],linklist[i],freelist[i]))
+			end
+		end
+		m = MPS(N,itensorlist,0,N+1)
+		new(m,freelist)
 	end
+
+	MPSState(N::Int) = MPSState(N,[1.,0.])
 end #struct
 
-getindex(st::MPSState,n::Int) = getindex(st.s,n)
-setindex!(st::MPSState,T::ITensor,n::Integer) = setindex!(st.s,T,n)
-length(m::MPSState) = m.s.N_
-copy(st::MPSState) = copy(st.s)
+# getindex(st::MPSState,n::Int) = getindex(st.s,n)
+# setindex!(st::MPSState,T::ITensor,n::Integer) = setindex!(st.s,T,n)
+# length(m::MPSState) = m.s.N_
+# copy(st::MPSState) = copy(st.s)
 
 
-function freeind(st::MPSState,j::Integer)
-	link =[]
-	if j<length(st)
-		push!(link,linkind(st.s,j)) #linkind not exported
-	end
-	if j>1
-		push!(link,linkind(st.s,j-1))
-	end
-	# print("get diff")
-	setdiff(inds(getindex(st,j)), link)
-end 
+# function freeind(st::MPSState,j::Integer)
+# 	link =[]
+# 	if j<length(st)
+# 		push!(link,linkind(st.s,j)) #linkind not exported
+# 	end
+# 	if j>1
+# 		push!(link,linkind(st.s,j-1))
+# 	end
+# 	# print("get diff")
+# 	setdiff(inds(getindex(st,j)), link)
+# end 
 
 
 
@@ -53,6 +73,11 @@ end
 # end 
 
 # ===== mini test ====
-st = MPSState(3)
-print(st)
-print(freeind(st,1))
+
+initstate = rand(Float64,2)
+# initstate/=norm(initstate)
+# print("norm = ", norm(initstate))
+print(MPSState(8,initstate))
+
+
+
