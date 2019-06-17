ac438660f66d05f409abb292f0e09bfab24e1036
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-06-16 21:23:22 -0400

    correct svd usage

--- helper.jl
+++ helper.jl
@@ -14,7 +14,7 @@ function exact_MPS(exact::ITensor,indexorder,
 			U,S,V,leftlink,v = svd(remain, indexorder[i];kwargs...)
 		end 
 		push!(resultMPS,U)
-		remain = S*V 
+		remain = S*dag(V) 
 	end
 	push!(resultMPS,remain)
 	return resultMPS
