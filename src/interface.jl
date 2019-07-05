9c76d99098fcecf62428d076ec1cbb206b8fe9cf
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-07-05 09:19:40 -0400

    deleted redundent file

--- src/interface.jl
+++ src/interface.jl
@@ -5,4 +5,4 @@
 (block::QGateBlock)(qubit::Int...) = (block, ActPosition([qubit...]))
 getindex(kernel::GateKernel, param::Real...) = VarGate(kernel, [param...])
 getindex(kernel::GateKernel, param::Vector{T} where {T<:Real}) = VarGate(kernel, param)
-(gate::ConstGate)(qubit::Int...) = (gate, ActPosition([qubit...])) # used as input arg in addGate!
+(gate::ConstGate)(qubit::Int...) = (gate, ActPosition([qubit...])) # used as input arg in add!
