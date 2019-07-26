28e1918c0cc0afc1fc4e6b94db2dc1950af2cb54
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-07-26 17:29:53 -0400

    add :probability to keyword argument list for measure!()

--- doc/qstates/mpsstate.jl
+++ doc/qstates/mpsstate.jl
@@ -246,6 +246,7 @@ Perform measurement to specified `qubits` in computatioal basis for `shots` time
 - `qubits`: vector of qubits to be measured
 - `shots`: number of measurements to be performed
 - `binary`: [_keyword argument_] If `binary == true`, the function returns results in binary form in a 2D array. Otherwise, the function returns measurement results in decimal form. Default value: `false`.  
+- `probability`: [_keyword argument_] whether or not to keep probability as a by-product.
 
 # Example
 ``` julia
@@ -274,6 +275,8 @@ julia> measure!(state, [1:3;], 5; binary = false)
  1
  1
 
+julia> measure!(state, [1:3;], 3; binary = true, probability = true)
+([0 0 0; 0 0 0; 1 0 0], [0.5, 0.5, 0.5])
 ``` 
  # Note
 Calling this function will not physically change state, but the internal data structure (e.g. the position of gauge center) of the MPS may be changed.  
@@ -344,6 +347,7 @@ Doing one measurement on state.
 
 This is a helper function called by **measure!**. The state needs to be pretreated (localize qubits to be measured & canonicalize) before calling this funciton. 
 
+# Note: Internal function.
 """ ->
 oneShot(state::MPSState, sites::Vector{Int}; kwargs...)
 
