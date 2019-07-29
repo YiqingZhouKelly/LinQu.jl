622f3f169a82d0b73564165d2eda3ecc47b0cef3
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-07-29 18:17:28 -0400

    add measure!() that takes keyword arguments; add test for measure with rng

--- src/interface.jl
+++ src/interface.jl
@@ -16,3 +16,19 @@ getindex(gate::VarGate, params::Vector) = (gate.param = params; return gate)
 (gate::ConstGate)(qubit::Vector{Int}) = (gate, qubit)
 
 apply!(state::QState, tup:: Tuple{Union{QGate, QGateBlock}, Vector{Int}} ; kwargs...) = apply!(state, tup[1], tup[2]; kwargs...)
+
+function measure!(state::QState; kwargs...)
+	qubits = get(kwargs, :qubits, nothing)
+	qubit = get(kwargs, :qubit, nothing)
+
+	shots = get(kwargs, :shots, 1024)
+	if qubits ==nothing
+		qubit == nothing && (throw(ArgumentError("Qubit(s) to be measured not specified")))
+		return measure!(state, qubits, shots; kwargs...)
+	else
+		qubit != nothing && (throw(ArgumentError("Both qubits and qubit are specified")))
+		return measure!(state, qubit, shots; kwargs...)
+	end
+end
+
+randomQCircuit(;size::Int, depth::Int = rand(1:10)) = QCircuit(randomQGateBlock(size,depth), size)
