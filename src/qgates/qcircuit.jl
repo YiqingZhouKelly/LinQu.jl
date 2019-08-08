5af4552f639625a793322c6fc8d230c45c7754ec
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-08-08 12:38:34 -0400

    extend add!

--- src/qgates/qcircuit.jl
+++ src/qgates/qcircuit.jl
@@ -6,17 +6,18 @@ struct QCircuit
 end # struct
 
 gates(circuit::QCircuit) = gate(circuit.block)
-size(circuit::QCircuit) = size(circuit.block) 
+size(circuit::QCircuit) = size(circuit.block)
 iterate(circuit::QCircuit, state::Int = 1) = iterate(circuit.block, state)
 length(circuit::QCircuit) = length(circuit.block)
 ==(circuit1::QCircuit, circuit2::QCircuit) = ((circuit1.block == circuit2.block) && (circuit1.N == circuit2.N))
 
 add!(circuit::QCircuit, operator::Operator, pos::Vector{Int}) = (add!(circuit.block, operator, pos); return circuit)
+add!(circuit::QCircuit, operator::Operator, qubits::Int...) = (add!(circuit.block,operator, [qubits...]); return circuit)
 add!(circuit::QCircuit, tuples::GatePosTuple...) = (add!(circuit.block, tuples...); return circuit)
 addCopy!(circuit::QCircuit, operator::Operator, pos::Vector{Int}) = (addCopy!(circuit.block, operator, pos); return circuit)
 addCopy!(circuit::QCircuit, tuples::GatePosTuple...) = (addCopy!(circuit.block, tuples...); return circuit)
 
-function flatten(circuit::QCircuit, flattened=nothing) 
+function flatten(circuit::QCircuit, flattened=nothing)
 	flattened = flatten(circuit.block, [1:circuit.N;], flattened)
 	return QCircuit(flattened, circuit.N)
 end
