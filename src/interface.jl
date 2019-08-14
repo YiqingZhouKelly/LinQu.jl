916757bee195238ac63abfa2a844a225cfb0c23e
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2019-08-13 21:50:03 -0500

    make alternative GATE() interface work with general QGate

--- src/interface.jl
+++ src/interface.jl
@@ -1,19 +1,17 @@
 
 # tuple 
 (kernel::GateKernel)(qubits::Int...) = QGate(kernel)(qubits...)
-(gate::VarGate)(qubit::Int...) = (gate, [qubit...])
+(gate::QGate)(qubit::Int...) = (gate, [qubit...])
 (block::QGateBlock)(qubit::Int...) = (block,[qubit...])
 getindex(kernel::GateKernel, param...) = VarGate(kernel, [param...])
 getindex(gate::VarGate, params...) = (gate.param = [params...]; return gate)
-(gate::ConstGate)(qubit::Int...) = (gate, [qubit...]) 
 
 #array
 (kernel::GateKernel)(qubits::Vector{Int}) = QGate(kernel)(qubits)
-(gate::VarGate)(qubit::Vector{Int}) = (gate, qubit)
+(gate::QGate)(qubit::Vector{Int}) = (gate, qubit)
 (block::QGateBlock)(qubit::Vector{Int}) = (block, qubit)
 getindex(kernel::GateKernel, param::Vector) = VarGate(kernel, param)
 getindex(gate::VarGate, params::Vector) = (gate.param = params; return gate)
-(gate::ConstGate)(qubit::Vector{Int}) = (gate, qubit)
 
 apply!(state::QState, tup:: Tuple{Union{QGate, QGateBlock}, Vector{Int}} ; kwargs...) = apply!(state, tup[1], tup[2]; kwargs...)
 
